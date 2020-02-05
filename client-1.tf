e636fb763024416df4341b2f63f189083bc91e6c
Murzik2142 <murzik2142@gmail.com>
2020-02-05 08:35:36 +0500

    change win to t2.small

--- client-1.tf
+++ client-1.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "VPC1_private_instance_win1" {
   subnet_id       = "${aws_subnet.Private_VPC1_Subnet.id}"
   key_name        = "${aws_key_pair.key.id}"
   security_groups = ["${aws_security_group.Tunnel_VPC1_SG.id}"]
-  instance_type   = "t2.micro"
+  instance_type   = "t2.small"
   tags = {
     Name = "Client-1-MMM"
     
--- client-2.tf
+++ client-2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "VPC2_private_instance-win2" {
   subnet_id       = "${aws_subnet.Private_VPC2_Subnet1.id}"
   key_name        = "${aws_key_pair.key.id}"
   security_groups = ["${aws_security_group.Tunnel_VPC2_SG.id}"]
-  instance_type   = "t2.micro"
+  instance_type   = "t2.small"
 
   tags = {
     Name = "Client-2-MMM"
